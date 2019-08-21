08fefcfa434682d401bb6bcd0f6affc0f2006e42
groland11 <github@rolandschnabel.de>
2019-08-21 05:38:58 +0200

    Copyright statement

--- gpg-cleanup.py
+++ gpg-cleanup.py
@@ -1,8 +1,35 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# coding: UTF-8
+#
+# Copyright (c) 2019
+#
+# Permission is hereby granted, free of charge, to any person 
+# obtaining a copy of this software and associated documentation 
+# files (the "Software"), to deal in the Software without 
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom 
+# the Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall 
+# be included in all copies or substantial portions of the 
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
 
+import sys, re, subprocess, time
+
+from os import access, W_OK
 from os.path import expanduser
 from pathlib import Path
-import sys, re, subprocess, time
 
 HOMEDIR = expanduser("~")
 GPGDIR  = HOMEDIR + "/.gnupg"
@@ -20,11 +47,15 @@ class PubKey:
 		self.uid       = uid
 
 def check_requirements():
+	# Check home directory
+	if not Path(HOMEDIR).is_dir(): 
+		sys.exit('ERROR: Invalid home directory "{}"'.format(HOMEDIR))
+
 	# Check gpg version
 
 	# Check gpg home directory
-	if not Path(GPGDIR).is_dir():
-		sys.exit('ERROR: Unable to find GnuPG home directory {}'.format(GPGDIR))
+	if not Path(GPGDIR).is_dir() or not access(GPGDIR, W_OK):
+		sys.exit('ERROR: Unable to access GnuPG home directory "{}"'.format(GPGDIR))
 
 	# Check gpg public key files
 
