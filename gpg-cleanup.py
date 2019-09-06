91b4caa453cc9d8bce7db2b61736b8a0be68942c
groland11 <github@rolandschnabel.de>
2019-09-06 22:28:28 +0200

    Added progressbar, multithreading

--- gpg-cleanup.py
+++ gpg-cleanup.py
@@ -30,7 +30,7 @@
 # - Print user ids in delete dialog
 # - Parameter for max. number of signatures
 
-import sys, re, subprocess, time
+import sys, os, re, subprocess, time, threading
 import argparse
 
 from os import access, W_OK
@@ -38,10 +38,10 @@ from os.path import expanduser
 from pathlib import Path
 from pubkey import Pubkey
 
-HOMEDIR = expanduser("~")
-GPGDIR  = HOMEDIR + "/.gnupg"
-GPGLOG  = HOMEDIR + "/pubkeys.cache"
-GNUPGHOME = ""
+HOMEDIR = expanduser('~')
+GPGDIR  = HOMEDIR + '/.gnupg'
+GPGLOG  = HOMEDIR + '/pubkeys.cache'
+GNUPGHOME = ''
 
 pubkeys = []
 delpubkeys = {}
@@ -53,9 +53,9 @@ def check_requirements():
 
 	# Check gpg version
 	try:
-		output = subprocess.check_output("gpg --version | egrep -i -o \"[0-9.]+\" | head -1", shell=True, universal_newlines=True, timeout=10)
+		output = subprocess.check_output('gpg --version | egrep -i -o "[0-9.]+" | head -1', shell=True, universal_newlines=True, timeout=10)
 		ver = output.strip()
-		if ver < "2.1":
+		if ver < '2.1':
 			sys.exit('ERROR: gpg version must be at least 2.1 (your version: {})'.format(ver))
 	except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
 		sys.exit('ERROR: Unable to retrieve gpg version: {}'.format(str(e)))
@@ -85,93 +85,132 @@ def deserialize(file_read=None, lines_read=None):
 			sys.exit('ERROR: Unable to read cache file {}: {}'.format(file_read, str(e)))
 	
 	if lines_read:
-		fpr = ""
+		fpr = ''
 		uids = []
 		for pubkey_line in lines_read:
 			match = re.search('^pub:', pubkey_line)
 			if match:
-				if fpr != "":
+				if fpr != '':
 					pubkeys_return.append(Pubkey(fpr, uids))
-				fpr = ""
+				fpr = ''
 				uids = []
 				continue
 			match = re.search('^fpr:::::::::([A-F0-9]+):', pubkey_line)
 			if match:
 				fpr = match.group(1)
 				continue
-			match = re.search("^uid:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:([^:]*):[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:", pubkey_line)
+			match = re.search('^uid:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:([^:]*):[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:', pubkey_line)
 			if match:
 				uids.append(match.group(1))
 				continue
 
-		if fpr != "":
+		if fpr != '':
 			pubkeys_return.append(Pubkey(fpr, uids))
 
 	return pubkeys_return
 
-# Check requirements, command line
+# Show progressbar
+def progressbar(statustext):
+	os.popen('tput civis').read()
+	i = 0
+	while lock1.acquire(blocking=False) == False:
+		print('{} '.format(statustext), end='')
+		for j in range(0, i):
+			print('.', end='')
+		print('', end='\r')
+		time.sleep(1)
+		i = i + 1
+	print('')
+	os.popen('tput cnorm').read()
+
+# Retrieve public keys from gpg
+def gpg_listkeys():
+	try:
+		proc = subprocess.run(['gpg', '--with-colons', '--list-keys'], stdout=subprocess.PIPE, check=True, timeout=args.timeout, encoding='utf-8')
+	except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
+		sys.exit('ERROR: Unable to list public keys: {}'.format(str(e)))
+
+	for pubkey_line in proc.stdout.split('\n'):
+		lines.append(pubkey_line)
+
+	#time.sleep(5)
+
+	lock1.release()
+
+# Check requirements, command line, initialize threading
 check_requirements()
 
 ap = argparse.ArgumentParser()
 group = ap.add_mutually_exclusive_group()
-ap.add_argument('-v', '--version', action='version', version='%(prog)s 0.1a', help="Show program's version and exit.")
-group.add_argument("-w", "--writecache", required=False, nargs='?', const=GPGLOG, help="Create cache file (default: '~/pubkeys.cache') and exit.")
-group.add_argument("-r", "--readcache", required=False, nargs='?', const=GPGLOG, help="Read from cache file (default: '~/pubkeys.cache').")
-ap.add_argument("-t", "--timeout", required=False, default=120, type=int, help="Timeout in seconds for gpg (default: 120).")
+ap.add_argument('-v', '--version', action='version', version='%(prog)s 0.1a', help='Show program\'s version and exit.')
+group.add_argument('-w', '--writecache', required=False, nargs='?', const=GPGLOG, help='Create cache file (default: \'~/pubkeys.cache\') and exit.')
+group.add_argument('-r', '--readcache', required=False, nargs='?', const=GPGLOG, help='Read from cache file (default: \'~/pubkeys.cache\').')
+ap.add_argument('-t', '--timeout', required=False, default=120, type=int, help='Timeout in seconds for gpg (default: 120).')
 args = ap.parse_args()
+
+lock1 = threading.Lock()
 
 # Write public keys to cache file and exit
 if args.writecache:
-	print("Creating cache file {} ...".format(args.writecache))
+
+	thread1 = threading.Thread(target=progressbar,  args=('Running gpg',))
+	thread2 = threading.Thread(target=gpg_listkeys)
+
+	lock1.acquire(blocking=False)
+
 	start_time = time.time()
-
-	try:
-		proc = subprocess.run(["gpg", "--with-colons", "--list-keys"], stdout=subprocess.PIPE, check=True, timeout=args.timeout, encoding='utf-8')
-	except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
-		sys.exit('ERROR: Unable to list public keys: {}'.format(str(e)))
-
 	lines = []
-	for pubkey_line in proc.stdout.split('\n'):
-		lines.append(pubkey_line)
 
+	thread1.start()
+	thread2.start()
+
+	thread1.join()
+	thread2.join()
+
+	print('Creating cache file {} ...'.format(args.writecache))
 	serialize(args.writecache, lines)
 
 	elapsed_time = time.time() - start_time
-	print("OK: The file {} now contains a list of all your public keys (elapsed time: {:.2f} sec)".format(args.writecache, elapsed_time))
+	print('OK: The file {} now contains a list of all your public keys (elapsed time: {:.2f} sec)'.format(args.writecache, elapsed_time))
 	sys.exit()
 
 # Retrieve public keys fingerprints from cache file
 if args.readcache:
 	if Path(args.readcache).is_file():
-		print("Using cache file {} ...".format(args.readcache))
+		print('Using cache file {} ...'.format(args.readcache))
 		pubkeys = deserialize(file_read=args.readcache)
 # Retrieve public keys fingerprints from gpg
 else:
-	try:
-		proc = subprocess.run(["gpg", "--with-colons", "--list-keys"], stdout=subprocess.PIPE, check=True, timeout=args.timeout, encoding='utf-8')
-	except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
-		sys.exit('ERROR: Unable to list public keys: {}'.format(str(e)))
+	thread1 = threading.Thread(target=progressbar,  args=('Running gpg',))
+	thread2 = threading.Thread(target=gpg_listkeys)
 
+	lock1.acquire(blocking=False)
+
+	start_time = time.time()
 	lines = []
-	for pubkey_line in proc.stdout.split('\n'):
-		lines.append(pubkey_line)
 
-	print(lines)
+	thread1.start()
+	thread2.start()
+
+	thread1.join()
+	thread2.join()
+
+	print('Processing gpg output ...')
 	pubkeys = deserialize(lines_read=lines)
 
 # Give some statistics
-print("You have {} keys in you public keyring.".format(len(pubkeys)))
-print("Getting signatures of those public keys ...")
+print('You have {} keys in you public keyring.'.format(len(pubkeys)))
+print('Getting signatures of those public keys ...')
 
 # List signatures for each public key to find out if key is suspicious
 for pubkey in pubkeys:
 	sig_count = 0
-	uid = ""
+	uid = ''
 	elapsed = 0.0
 
 	try:
 		start = time.time()
-		proc = subprocess.run(["gpg", "--list-sig", pubkey.fpr], stdout=subprocess.PIPE, check=True, timeout=args.timeout, encoding='utf-8')
+		proc = subprocess.run(['gpg', '--list-sig', pubkey.fpr], stdout=subprocess.PIPE, check=True, timeout=args.timeout, encoding='utf-8')
 		elapsed = time.time() - start
 		for sigs_line in proc.stdout.split('\n'):
 			match = re.search('^sig[ \t]+', sigs_line)
@@ -202,9 +241,9 @@ else:
 	for fpr in delpubkeys:
 		print('Public key with suspicous signatures: {} ({} signatures listed in {:.2f} sec)'.format(fpr, delpubkeys[fpr].sigcount, delpubkeys[fpr].elapsed))
 		ret = input('Do you want to delete this key? [y|N] >')
-		if ret.lower() == "y":
+		if ret.lower() == 'y':
 			try:
-				proc = subprocess.run(["gpg", "--delete-keys", fpr], stdout=subprocess.PIPE, check=True, timeout=180, encoding='utf-8')
+				proc = subprocess.run(['gpg', '--delete-keys', fpr], stdout=subprocess.PIPE, check=True, timeout=180, encoding='utf-8')
 			except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
 				print('ERROR: Unable to delete key {}: {}'.format(fpr, str(e)))
 				pass
